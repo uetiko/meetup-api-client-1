73908739fb6d24a92e3986aa4711aaf35d944601
Angel Barrientos <uetiko@gmail.com>
2020-07-14 22:01:13 -0500

    Added the oauth provider and conficuration object.

---
+++ src/Api/Client/Domain/Config.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Meetup\Api\Client\Domain;
+
+use ArrayObject;
+
+final class Config
+{
+    /** @var ArrayObject $array */
+    private $arrayConfig = null;
+    /** @var string $clientId */
+    private $clientId = null;
+    /** @var string $clientSecret */
+    private $clientSecret = null;
+    /** @var string $redirectUri */
+    private $redirectUri = null;
+    /** @var string $state */
+    private $state = null;
+    /** @var string $authorizationUrl */
+    private $authorizationUrl = null;
+    /** @var string $accessToken */
+    private $accessToken = null;
+
+    /**
+     * Config constructor.
+     * @param string $clientId
+     * @param string $clientSecret
+     * @param string $redirectUri
+     */
+    public function __construct(string $clientId, string $clientSecret, string $redirectUri)
+    {
+        $this->clientId = $clientId;
+        $this->clientSecret = $clientSecret;
+        $this->redirectUri = $redirectUri;
+        $this->staticBiuldUrlAccess();
+        $this->buildArrayConfig();
+    }
+
+    protected function staticBiuldUrlAccess()
+    {
+        $this->authorizationUrl = 'https://secure.meetup.com/oauth2/authorize';
+        $this->accessToken = 'https://secure.meetup.com/oauth2/access';
+    }
+
+    protected function buildArrayConfig(): void
+    {
+        $this->array = new ArrayObject([
+            'clientId' => $this->getClientId(),
+            'clientSecret' => $this->getClientSecret(),
+            'redirectUri' => $this->getRedirectUri(),
+            'urlAuthorize' => $this->getAuthorizationUrl(),
+            'urlAccessToken' => $this->getAccessToken()
+        ]);
+    }
+
+    /**
+     * @return array
+     */
+    public function getArrayConfig(): array
+    {
+        return $this->arrayConfig->getArrayCopy();
+    }
+
+    /**
+     * @return string
+     */
+    public function getClientId(): string
+    {
+        return $this->clientId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClientSecret(): string
+    {
+        return $this->clientSecret;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRedirectUri(): string
+    {
+        return $this->redirectUri;
+    }
+
+    /**
+     * @return string
+     */
+    public function getState(): string
+    {
+        return $this->state;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthorizationUrl(): string
+    {
+        return $this->authorizationUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAccessToken(): string
+    {
+        return $this->accessToken;
+    }
+}
---
+++ src/Api/Client/Infrastructure/OAuthMeetup.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Meetup\Api\Client\Infrastructure;
+
+use League\OAuth2\Client\Provider\AbstractProvider;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+use League\OAuth2\Client\Token\AccessToken;
+use Meetup\Api\Client\Domain\Config;
+use Psr\Http\Message\ResponseInterface;
+
+class OAuthMeetup extends AbstractProvider
+{
+    /** @var Config $config */
+    private $config = null;
+    public function __construct(Config $config)
+    {
+        $this->config = $config;
+        parent::__construct($config->getArrayConfig());
+    }
+
+    /**
+     * @return string
+     */
+    public function getBaseAuthorizationUrl()
+    {
+        return $this->config->getAuthorizationUrl();
+    }
+
+    /**
+     * @param array $params
+     * @return string
+     */
+    public function getBaseAccessTokenUrl(array $params)
+    {
+        return $this->config->getAccessToken();
+    }
+
+    /**
+     * @param AccessToken $token
+     * @return string
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
+        $tail = urldecode(http_build_query(
+            [
+                'access_token' => $token->getToken()
+            ]
+        ));
+
+        return sprintf('https://api.meetup.com/2/member/self?%s', $tail);
+    }
+
+    /**
+     * @return array
+     */
+    protected function getDefaultScopes()
+    {
+        // TODO: Implement getDefaultScopes() method.
+    }
+
+    /**
+     * @param ResponseInterface $response
+     * @param array|string $data
+     * @throws IdentityProviderException
+     */
+    protected function checkResponse(ResponseInterface $response, $data): void
+    {
+        if (!is_array($data)) {
+            throw new IdentityProviderException(
+                'Invalid Response, a array was expected'
+            );
+        }
+
+        if (200 !== $response->getStatusCode()) {
+            throw new IdentityProviderException(
+                'Invalid status code, a 200 code was expected'
+            );
+        }
+    }
+
+    /**
+     * @param array $response
+     * @param AccessToken $token
+     * @return ResourceOwnerInterface
+     */
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+    }
+}
